c0edffeca33ef6329815825d252a5370d8c70d50
LukasJ00 <lukas@janfelt.se>
2023-04-26 15:02:56 +0200

    new

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// skapar en container 
+// container 
 const container = document.createElement('container');
 document.body.append(container);
 
@@ -14,23 +14,24 @@ for(let i=1; i<=5; i++){
     div.style.backgroundColor = `hsl(${120+(i*15)}, 90%, 85%)`;
 }
 
-//skapar en section 
+//section 
 const section = document.createElement('section');
 document.body.append(section);
 section.style.border = '2px solid black';
 section.style.display ='flex'
+section.style.justifyContent = 'space-evenly'
 section.style.width = "100%";
 
-//skapa kort 1
+//kort 1
 const card1 = document.createElement('div');
 section.appendChild(card1);
 card1.style.width = '100px'
 card1.style.display = 'flex';
 card1.style.justifyContent = 'center';
-card1.style.margin = '3rem 20%'
+card1.style.margin = '3rem'
 card1.style.backgroundColor = 'hsl(240, 71%, 80%)';
 
-//skapa kolumn 1 
+//kolumn 1 
 const column1 =  document.createElement('div');
 card1.appendChild(column1);
 column1.style.backgroundColor = 'white'
@@ -57,16 +58,16 @@ if (j === 4) {
   column1.appendChild(number);
 }
 
-//skapa kort 2
+//kort 2
 const card2 = document.createElement('div');
 section.appendChild(card2);
 card2.style.width = '100px'
 card2.style.display = 'flex';
 card2.style.justifyContent = 'center';
-card2.style.margin = '3rem 20%'
+card2.style.margin = '3rem'
 card2.style.backgroundColor = 'hsl(240, 71%, 80%)';
 
-//skapar kulumn 2 
+//kulumn 2 
 const column2 =  document.createElement('div');
 card2.appendChild(column2);
 column2.style.backgroundColor = 'white'
@@ -91,3 +92,34 @@ for (let x=9;  x>=0; x--){
     }
     column2.appendChild(number);
     }
+
+// kort 3
+const card3 = document.createElement('div');
+section.appendChild(card3);
+card3.style.width = '100px'
+card3.style.display = 'flex';
+card3.style.justifyContent = 'center';
+card3.style.margin = '3rem'
+card3.style.backgroundColor = 'hsl(240, 71%, 80%)';
+
+//kulumn 3
+const column3 =  document.createElement('div');
+card3.append(column3);
+column3.style.backgroundColor = 'white'
+column3.style.width = '70px'
+column3.style.margin = '10px'
+
+//nummer array
+const text = ['ett', 'två', 'tre', 'fyra', 'fem', 'sex', 'sju', 'åtta', 'nio', 'tio'];
+
+for (let i=0; i<text.length; i++){
+    const paragraph = document.createElement('p'); 
+    paragraph.innerText = text[i];
+    paragraph.style.fontSize = '1.5rem';
+    paragraph.style.textAlign = 'right';
+    paragraph.style.margin = '0'; 
+    column3.append(paragraph) 
+    if (i==='sex'){
+        paragraph.Color = 'hsl(240, 71%, 80%)'
+    }
+}
