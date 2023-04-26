64ba19db64e491103a093e41dfc34aca91d0f89c
LukasJ00 <lukas@janfelt.se>
2023-04-26 12:57:52 +0200

    new

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const container = document.createElement('container');
 document.body.append(container);
 
- 
 for(let i=1; i<=5; i++){
     const div = document.createElement('div');
     container.append(div);
@@ -11,9 +10,10 @@ for(let i=1; i<=5; i++){
     div.style.display ="flex";
     div.style.justifyContent = "center";
     div.style.margin = `${5*i}px 0`;
-    div.style.color = `hsl(240, 90%, 70%)`;
+    div.style.color = 'hsl(240, 90%, 70%)';
     div.style.backgroundColor = `hsl(${120+(i*15)}, 90%, 85%)`;
 }
+
 //skapar en section 
 const section = document.createElement('section');
 document.body.append(section);
@@ -26,18 +26,17 @@ section.appendChild(card1);
 card1.style.width = '100px'
 card1.style.display = 'flex';
 card1.style.justifyContent = 'center';
+card1.style.margin = '3rem 20%'
 card1.style.backgroundColor = 'hsl(240, 71%, 80%)';
 
-//skapa column i kort
+//skapa column1 i kort
 const column1 =  document.createElement('div');
 card1.appendChild(column1);
 column1.style.backgroundColor = 'white'
 column1.style.width = '70px'
 column1.style.margin = '10px '
 
-
-
-//numren
+//nummer loop
 for (let j=0;  j<=9; j++){
 const number = document.createElement('div');
 number.textContent = j;
